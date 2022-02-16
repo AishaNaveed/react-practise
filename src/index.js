7b0f00a82d2461061fe641a4f8b137b1186465f6
AishaNaveed <aisha@ailogica.com>
2022-02-16 21:49:05 +0000

    starting implementing props

--- src/index.js
+++ src/index.js
@@ -2,37 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const bookName = "Love You to the Moon and Back";
+const writer = "By: (Alex Clark Art)";
+const pic =
+  "https://cdn.shopify.com/s/files/1/0468/6734/9662/products/Coaster-LoveyouToThemoonandback_1024x1024@2x.jpg?v=1637598287";
+
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book designation="software engineer" />
     </section>
   );
 }
 
-const Book = () => (
+const Book = (props) => (
   <article className="book">
     <Image />
     <Title />
     <Author />
+    <p>{props.job}</p>
+    <p>{props.designation}</p>
+    {/* <p>{console.log(props)}</p> */}
   </article>
 );
 
-const Image = () => (
-  <img
-    src="https://cdn.shopify.com/s/files/1/0468/6734/9662/products/Coaster-LoveyouToThemoonandback_1024x1024@2x.jpg?v=1637598287"
-    alt=""
-  />
-);
-const bookName = "Love You to the Moon and Back";
-
+const Image = () => <img src={pic} alt="" />;
 const Title = () => <h1>{bookName}</h1>;
-
-const Author = () => {
-  const writer = "By: (Alex Clark Art)";
-  return <h5>{writer}</h5>;
-};
+const Author = () => <h5>{writer.toLocaleUpperCase()}</h5>;
 
 // const Author = () => (
 //   <h5 style={{ color: "crimson", fontFamily: "cursive" }}>
